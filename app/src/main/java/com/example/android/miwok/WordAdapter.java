170dd4f637eb7f03bf6da1c53aedb4080a22d09d
Alena Kosheleva <alena@192.168.1.100>
2018-01-10 19:11:51 +0000

    Modify WordAdapter class.

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -36,6 +37,12 @@ public class WordAdapter extends ArrayAdapter<Word>{
         // Get the version name from the current Word object and
         // set this text on the name TextView
         defaultWordTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID version_number
+        ImageView image = listItemView.findViewById(R.id.imageView);
+        // Get the version name from the current Word object and
+        // set this image on the name ImageView
+        image.setImageResource(currentWord.getImageRecourceId());
 
         return listItemView;
     }
